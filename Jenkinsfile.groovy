edc9ac95f8807f68511b3197c97b664090e1c24b
Dinesh Kumaran <d,kumaran.m@gmail.com>
2021-10-16 16:47:31 +0530

    added shell

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,5 +6,11 @@ pipeline {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'DineshK', url: 'https://github.com/DineshKumaran91/05_Class_PipeSlave.git']]])
             }
         }
+        stage('Execute shell') {
+            steps {
+                sh '''echo "This is my job"
+                echo $JOB_NAME'''
+            }
+        }
     }
 }
