ac3af79f2a0b87cc26a2ffbd579003088b802b7d
Dinesh Kumaran <d,kumaran.m@gmail.com>
2021-10-16 16:16:11 +0530

    adding jenkings file

---
+++ JenkinsFile/Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'DineshK', url: 'https://github.com/DineshKumaran91/5_Class_PipeSlave/tree/master/simple-java-maven-app']]])
+            }
+        }
+        stage('Test') {
+            steps {
+                //
+            }
+        }
+        stage('Deploy') {
+            steps {
+                //
+            }
+        }
+    }
+}
