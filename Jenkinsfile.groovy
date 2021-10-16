fea093198e693cd5c131a2fc6a5957602c0088be
Dinesh Kumaran <d,kumaran.m@gmail.com>
2021-10-16 16:37:06 +0530

    checkout repo added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,15 +6,6 @@ pipeline {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'DineshK', url: 'https://github.com/DineshKumaran91/5_Class_PipeSlave/tree/master/simple-java-maven-app']]])
             }
         }
-        stage('Test') {
-            steps {
-                //
-            }
-        }
-        stage('Deploy') {
-            steps {
-                //
-            }
-        }
+
     }
 }
